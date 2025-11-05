5e2c32f21e8ab34cf07ed21d873a52a6d3a86549
hossein ebdalzadeh <hossein.ebdalzadeh@gmail.com>
2025-11-05 17:17:39 +0330

    adding resolve for using other HTML files when i build the website dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'path'; // for use resolve, then tell vite about another HTML files
+
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "./",
+  build: {
+    rollupOptions: {
+      // 👇 tell Vite about ALL your HTML entry points
+      input: {
+        main:      resolve(__dirname, 'index.html'),
+        fantastic: resolve(__dirname, 'fantastic-page.html'),
+        register:  resolve(__dirname, 'register-page.html'),
+      },},}
 });
